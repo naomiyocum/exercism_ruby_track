5c2ce6fe45305a4d494ced78d736c30051f3b48f
Naomi Yocum <naomiyocum24@gmail.com>
2023-04-26 17:35:25 -0600

    Complete matrix exercise

---
+++ instance_variables/matrix.rb
@@ -0,0 +1,19 @@
+class Matrix
+  attr_reader :matrix
+  
+  def initialize(matrix)
+    @matrix = matrix
+  end
+
+  def rows
+    matrix.each_line.map do |row|
+      row.split.map(&:to_i)
+    end
+  end
+
+  def columns
+    rows.transpose
+  end
+end
+
+
