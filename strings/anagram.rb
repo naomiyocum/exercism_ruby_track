da4fbe32d294629494f9d7df230efc2c8e04c65e
Naomi Yocum <naomiyocum24@gmail.com>
2023-04-27 08:50:59 -0600

    Complete anagram exercise

---
+++ strings/anagram.rb
@@ -0,0 +1,11 @@
+class Anagram
+  attr_reader :word
+  
+  def initialize(word)
+    @word = word.downcase
+  end
+
+  def match(arr_words)
+    arr_words.filter_map {|w| w if w.downcase.chars.sort == word.chars.sort && w != word}
+  end
+end
