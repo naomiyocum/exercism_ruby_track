888cca33e521ce95ce2bc1d46da22eeb63be455a
Naomi Yocum <naomiyocum24@gmail.com>
2023-05-02 08:53:55 -0600

    complete scrabble

---
+++ advanced_enumeration/scrabble.rb
@@ -0,0 +1,23 @@
+class Scrabble
+  attr_reader :word
+  
+  def initialize(word)
+    @word = word.upcase
+  end
+  
+  def score_board
+    {
+      /[AEIOULNRST]/ => 1,
+      /[DG]/ => 2,
+      /[BCMP]/ => 3,
+      /[FHVWY]/ => 4,
+      /[K]/ => 5,
+      /[JX]/ => 8,
+      /[QZ]/ => 10
+    }
+  end
+
+  def score
+    score_board.sum {|letter, value| word.scan(letter).count * value} 
+  end
+end
