b3fd76c5d371be5b9c1891d47bb1b00f8339c0d7
Naomi Yocum <naomiyocum24@gmail.com>
2023-05-23 08:11:29 -0600

    add community solution w regex

--- numbers/phone_number.rb
+++ numbers/phone_number.rb
@@ -10,3 +10,11 @@ class PhoneNumber
     clean_num
   end
 end
+
+class PhoneNumber
+  VALID_PHONE_NUMBER = /^([2-9]\d\d){2}\d{4}$/
+  def self.clean(number)
+    numbers = number.gsub(/\D/, "").sub(/^1/, "")
+    numbers[VALID_PHONE_NUMBER]
+  end
+end
